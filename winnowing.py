c0ab46dd6d4810d8c3abab3226390888ef4044e8
aryanguptaSG <aryan31102001@gmail.com>
2021-04-07 11:38:18 +0530

    third commit

--- winnowing.py
+++ winnowing.py
@@ -46,26 +46,26 @@ class winnowing():
         points = points[1:]
         mergedPoints = []
         mergedPoints.append(points[0])
-        for i in range(1, len(points)):
+        for i in points:
             last = mergedPoints[-1]
-            if points[i][0] >= last[0] and points[i][0] <= last[1]: #merging overlapping regions
-                if points[i][1] > last[1]:
+            if i[0] >= last[0] and i[0] <= last[1]: #merging overlapping regions
+                if i[1] > last[1]:
                     mergedPoints = mergedPoints[:-1]
-                    mergedPoints.append([last[0], points[i][1]])
+                    mergedPoints.append([last[0], i[1]])
                 else:
                     pass
             else:
-                mergedPoints.append(points[i])
+                mergedPoints.append(i)
         newCode = code[: mergedPoints[0][0]]
         plagCount = 0
-        for i in range(len(mergedPoints)):
-            if mergedPoints[i][1] > mergedPoints[i][0]:
-                plagCount += mergedPoints[i][1] - mergedPoints[i][0]
-                newCode = newCode + '\x1b[6;30;42m' + code[mergedPoints[i][0] : mergedPoints[i][1]] + '\x1b[0m'
+        for i,value in enumerate(mergedPoints):
+            if value[1] > value[0]:
+                plagCount += value[1] - value[0]
+                newCode = newCode + '\x1b[6;30;42m' + code[value[0] : value[1]] + '\x1b[0m'
                 if i < len(mergedPoints) - 1:
-                    newCode = newCode + code[mergedPoints[i][1] : mergedPoints[i+1][0]]
+                    newCode = newCode + code[value[1] : mergedPoints[i+1][0]]
                 else:
-                    newCode = newCode + code[mergedPoints[i][1] :]
+                    newCode = newCode + code[value[1] :]
         return {"ratio":(plagCount/len(code)),"Code":newCode}
 
     #we form windows of hash values and use min-hash to limit the number of fingerprints
@@ -86,11 +86,8 @@ class winnowing():
         return fingerprintList
     #takes k-gram list as input and returns a list of only hash values
     def hashList(self,arr):
-        HL = []
-        for i in arr:
-            HL.append(i[1])
-
-        return HL
+        return [i[1] for i in arr]
+        
     #function to form k-grams out of the cleaned up text
     def kgrams(self,text, k = 25):
         tokenList = list(text)
@@ -106,10 +103,9 @@ class winnowing():
     def minIndex(self,arr):
         minI = 0
         minV = arr[0]
-        n = len(arr)
-        for i in range(n):
-            if arr[i] < minV:
-                minV = arr[i]
+        for i,value in enumerate(arr):
+            if value < minV:
+                minV = value
                 minI = i
         return minI
 
@@ -120,6 +116,5 @@ class winnowing():
         hashval = hashval.hexdigest()[-4 :]
         hashval = int(hashval, 16)  #using last 16 bits of sha-1 digest
         return hashval
-
 
 
