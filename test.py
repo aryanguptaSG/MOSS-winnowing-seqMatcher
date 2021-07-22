c0ada26fed5340eabc57394ae35d73a5dad5d36e
aryanguptaSG <aryan31102001@gmail.com>
2021-07-22 19:23:13 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,47 +1,54 @@
+from posixpath import abspath
 import unittest
 import winnowing
+from os import path
+
+
+# getting the path of file 
+def setUp(file):
+    return path.abspath(f'tests/{file}')
 
 
 class TestWinnowing(unittest.TestCase):
     # testing for R language
     def test1(self):
-        check = winnowing.Winnowing(file1= "./tests/test6.r",file2="./tests/test7.r",lang="r")
+        check = winnowing.Winnowing(file1=setUp("test6.r"),file2=setUp("test7.r"),lang="r")
         self.assertEqual(round(check.jaccardCheck(),2),0.91)
         self.assertEqual(round(check.plagiarismRate(),2),0.91)
 
     # testing for Python language
     def test2(self):
-        check = winnowing.Winnowing(file1= "./tests/test2.py",file2="./tests/test3.py",lang="py")
+        check = winnowing.Winnowing(file1= setUp("test2.py"),file2=setUp("test3.py"),lang="py")
         self.assertEqual(round(check.jaccardCheck(),2),0.57)
         self.assertEqual(round(check.plagiarismRate(),2),0.57)
     
     # testing for C++ language
     def test3(self):
-        check = winnowing.Winnowing(file1= "./tests/test4.cpp",file2="./tests/test5.cpp",lang="cpp")
+        check = winnowing.Winnowing(file1=setUp("test4.cpp"),file2=setUp("test5.cpp"),lang="cpp")
         self.assertEqual(round(check.jaccardCheck(),2),0.57)
         self.assertEqual(round(check.plagiarismRate(),2),0.57)
 
     # testing for Java language
     def test4(self):
-        check = winnowing.Winnowing(file1= "./tests/test8.java",file2="./tests/test9.java",lang="java")
+        check = winnowing.Winnowing(file1= setUp("test8.java"),file2=setUp("test9.java"),lang="java")
         self.assertEqual(round(check.jaccardCheck(),2),1.00)
         self.assertEqual(round(check.plagiarismRate(),2),1.00)
 
     # testing with different type of files
     def test5(self):
-        self.assertRaises(Exception,winnowing.Winnowing,file1="./tests/test2.py",file2="./tests/test8.java",lang="py")
+        self.assertRaises(Exception,winnowing.Winnowing,file1=setUp("test2.py"),file2=setUp("test8.java"),lang="py")
 
     # testing with empty files
     def test6(self):
-        self.assertRaises(Exception,winnowing.Winnowing,file1="./tests/test1.txt",file2="./tests/test1.txt",lang="r")
+        self.assertRaises(Exception,winnowing.Winnowing,file1=setUp("test1.txt"),file2=setUp("test1.txt"),lang="r")
         
     # testing without passing language 
     def test6(self):
-        self.assertRaises(Exception,winnowing.Winnowing,file1="./tests/test2.py",file2="./tests/test3.py")
+        self.assertRaises(Exception,winnowing.Winnowing,file1=setUp("test2.py"),file2=setUp("test3.py"))
 
     # testing with invalid arguments
     def test7(self):
-        self.assertRaises(Exception,winnowing.Winnowing,rendom="./tests/test2.py",file2="./tests/test3.py",language="py")
+        self.assertRaises(Exception,winnowing.Winnowing,rendom=setUp("test2.py"),file2=setUp("test3.py"),language="py")
 
 
 
